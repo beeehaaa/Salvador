1f509f9764a3e03758b5e7981e832a734b140430
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2021-06-04 20:02:50 +0500

    Added User Screen & Scrolling Refresh

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -3,36 +3,98 @@
 /* eslint-disable semi */
 /* eslint-disable keyword-spacing */
 /* eslint-disable prettier/prettier */
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
-import { TextInput, Button } from 'react-native-paper';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, ScrollView, FlatList, Image, TouchableOpacity, KeyboardAvoidingView, Linking, Platform } from 'react-native';
+import { TextInput, Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 export default function AccountScreen(props) {
+    const [items, setItems] = useState([])
+    const [loading, setLoading] = useState(false)
+    const getDetails = async () => {
+        const querySnap = await firestore().collection('ads')
+            .where('uid', '==', auth().currentUser.uid)
+            .get()
+        const result = querySnap.docs.map(docSnap => docSnap.data())
+        setItems(result)
+    }
+    useEffect(() => {
+        getDetails()
+        return () => {
+            console.log('Clean Up');
+        }
+    }, [])
+    const openDial = (phone) => {
+        if (Platform.OS === 'android') {
+            Linking.openURL(`tel:${phone}`)
+        }
+        else {
+            Linking.openURL(`telprompt:${phone}`)
+        }
+    }
+    const renderIntem = (item) => {
+        return (
+            <Card style={styles.card}>
+                <Card.Title title={item.name} />
+                <Card.Content>
+                    <Paragraph>{item.desc}</Paragraph>
+                </Card.Content>
+                <Card.Cover source={{ uri: item.image }} />
+                <Card.Actions>
+                    <Button>Adopt Me </Button>
+                    <Button onPress={() => openDial()}>Call Seller</Button>
+                </Card.Actions>
+            </Card>
+        )
+    }
     return (
-        <View style={styles.imageContainer}>
-            <Image style={styles.imgTag} source={require('../assets/mainLogo.png')} />
-            <Text style={styles.textTag}> {auth().currentUser.email} </Text>
+        <View style={styles.viewMain}>
+            <View style={styles.imageContainer}>
+                <Text style={styles.textTag}> {auth().currentUser.email} </Text>
 
-            <Button
-          style={styles.btnStyle}
-          mode="contained"
-          onPress={() => auth().signOut()}
-        >
-          LOG OUT
-        </Button>
+
+                <Button
+                    style={styles.btnStyle}
+                    mode="contained"
+                    onPress={() => auth().signOut()}
+                >
+                    LOG OUT
+                </Button>
+                <Text style={styles.textTag}> Your Posted Ads!</Text>
+            </View>
+            <View>
+                <FlatList
+                    data={items}
+                    keyExtractor={(item) => item.contactNumber}
+                    renderItem={({ item }) => renderIntem(item)}
+                   
+                    onRefresh={() => {
+                        setLoading(true)
+                        getDetails()
+                        setLoading(false)
+                    }}
+                    refreshing={loading}
+                />
+            </View>
+
+
         </View>
-  );
+    );
 }
 //justify-content for column AND align-items for rows
 const styles = StyleSheet.create({
     imageContainer: {
         alignItems: 'center',
-        marginTop: 70,
+
+        justifyContent: 'space-evenly',
     },
     imgTag: {
         width: 200,
         height: 200,
 
+    },
+    viewMain: {
+        flex: 1,
     },
     textInpStyle: {
         marginLeft: 18,
@@ -43,7 +105,7 @@ const styles = StyleSheet.create({
     btnStyle: {
         marginLeft: 18,
         marginRight: 18,
-        marginTop: 30,
+        marginTop: 10,
         backgroundColor: '#381f47',
         textShadowColor: 'white',
         borderRadius: 35,
--- src/screens/ListItemScreen.js
+++ src/screens/ListItemScreen.js
@@ -5,51 +5,30 @@
 /* eslint-disable prettier/prettier */
 
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, ScrollView, FlatList, Image, TouchableOpacity, KeyboardAvoidingView,Linking,Platform } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, FlatList, Image, TouchableOpacity, KeyboardAvoidingView, Linking, Platform } from 'react-native';
 import { TextInput, Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
 import firestore from '@react-native-firebase/firestore';
 
 const ListItemScreen = () => {
-    const myitems = [
-        {
-            name: 'Adopt chow chow',
-            type: 'cat',
-            breed: 'persian',
-            age: '3',
-            description: 'moving out dye to the megerncy cant but mre pets in my',
-            contactNumber: '03229437619',
-            contactName: 'abdullah',
-            image: 'https://images.unsplash.com/photo-1556656793-08538906a9f8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aXBob25lfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
-        },
-        {
-            name: 'Milk',
-            type: 'dog',
-            breed: 'german',
-            age: '8',
-            description: 'Dont have time',
-            contactNumber: '03334720687',
-            contactName: 'abdullah',
-            image: 'https://images.unsplash.com/photo-1511044568932-338cba0ad803?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
-        },
-    ]
     const [items, setItems] = useState([])
+    const [loading, setLoading] = useState(false)
     const getDetails = async () => {
         const querySnap = await firestore().collection('ads').get()
         const result = querySnap.docs.map(docSnap => docSnap.data())
         console.log(result)
         setItems(result)
     }
-    const openDial = (phone) =>{
-        if(Platform.OS === 'android'){
+    const openDial = (phone) => {
+        if (Platform.OS === 'android') {
             Linking.openURL(`tel:${phone}`)
         }
-        else{
+        else {
             Linking.openURL(`telprompt:${phone}`)
         }
     }
     useEffect(() => {
         getDetails()
-        return ()=>{
+        return () => {
             console.log('Clean Up');
         }
     }, [])
@@ -63,7 +42,7 @@ const ListItemScreen = () => {
                 <Card.Cover source={{ uri: item.image }} />
                 <Card.Actions>
                     <Button>Adopt Me </Button>
-                    <Button onPress={()=>openDial()}>Call Seller</Button>
+                    <Button onPress={() => openDial()}>Call Seller</Button>
                 </Card.Actions>
             </Card>
         )
@@ -75,7 +54,13 @@ const ListItemScreen = () => {
                 data={items}
                 keyExtractor={(item) => item.contactNumber}
                 renderItem={({ item }) => renderIntem(item)}
-                inverted={true}
+                inverted={false}
+                onRefresh={() => {
+                    setLoading(true)
+                    getDetails()
+                    setLoading(false)
+                }}
+                refreshing={loading}
             />
         </View>
     )
